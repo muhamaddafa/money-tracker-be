c18d02465d99fc62252d598a82c6b16995db9d9c
muhamaddafa <gaming102dafa@gmail.com>
2025-10-20 22:18:24 +0700

    .

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
+import { api } from "./modules/api";
+
+const app = new Elysia()
+  .use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"]
+  }))
+  .use(api)
+  .listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
