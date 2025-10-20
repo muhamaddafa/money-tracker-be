c385cef41e5e73714a99185825590bed9982baf3
muhamaddafa <gaming102dafa@gmail.com>
2025-10-20 22:22:10 +0700

    deploy

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,9 @@ import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
 import { api } from "./modules/api";
 
-const app = new Elysia()
+export default new Elysia()
   .use(cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"]
   }))
-  .use(api)
-  .listen(3000);
-
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+  .use(api);
