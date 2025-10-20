ec08dcb835de50a0ec340c806c3a6f103c075826
muhamaddafa <gaming102dafa@gmail.com>
2025-10-20 21:17:07 +0700

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
+import { api } from "./modules/api";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"]
+  }))
+  .use(api)
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
