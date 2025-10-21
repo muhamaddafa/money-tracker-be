e654ca6d52c3f83051b95331158f69d1dc3b4624
muhamaddafa <gaming102dafa@gmail.com>
2025-10-21 22:48:37 +0700

    serverless

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
 import { api } from "./modules/api";
 
-export default new Elysia()
+const app = new Elysia()
   .use(cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"]
   }))
-  .use(api)
-  .listen(4000, () => console.log("Server running on http://localhost:4000"));
+  .use(api);
+
+// Export for Vercel serverless
+export default app.fetch;
