5a872c3eb6f9621ffb1d620e98cb6a2f0fe0e64f
muhamaddafa <gaming102dafa@gmail.com>
2025-10-29 22:15:33 +0700

    s

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { Elysia } from "elysia";
 import { api } from "./modules/api";
 
 new Elysia()
+	.get("/", () => "Welcome to the Bun Elysia API!")
   .use(api)
   .listen(process.env.PORT ?? 8080);
