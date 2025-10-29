428a84b62c6fbd6918e388adf62e365ebaba0718
muhamaddafa <gaming102dafa@gmail.com>
2025-10-29 22:16:29 +0700

    s

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { Elysia } from "elysia";
 import { api } from "./modules/api";
 
-new Elysia()
+const app = new Elysia()
 	.get("/", () => "Welcome to the Bun Elysia API!")
   .use(api)
   .listen(process.env.PORT ?? 8080);
+
+console.log(`Server running at http://localhost:${app.server?.port}`);
+
+export { app };
