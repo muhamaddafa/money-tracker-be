50bbfd7456802c54efb5a2a42f6d306f85fa839c
muhamaddafa <gaming102dafa@gmail.com>
2025-10-29 22:03:36 +0700

    fix: vercel deploy

--- src/modules/transactions/model.ts
+++ src/modules/transactions/model.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const addTransactionSchema = z.object({
-  accountId: z.number(),
-  categoryId: z.number(),
+  accountId: z.string(),
+  categoryId: z.string(),
   description: z.string(),
   amount: z.number(),
   trxType: z.enum(["income", "expense"])
@@ -11,16 +11,16 @@ export const addTransactionSchema = z.object({
 export const getTransactionListQuerySchema = z.object({
   description: z.string().optional(),
   trxType: z.enum(["income", "expense"]).optional(),
-  categoryId: z.number().optional(),
-  accountId: z.number().optional(),
+  categoryId: z.string().optional(),
+  accountId: z.string().optional(),
   pageNumber: z.number().optional().default(1),
   pageSize: z.number().optional().default(10)
 });
 
 export const editTransactionSchema = z.object({
-  transactionId: z.number(),
-  accountId: z.number().optional(),
-  categoryId: z.number().optional(),
+  transactionId: z.string(),
+  accountId: z.string().optional(),
+  categoryId: z.string().optional(),
   description: z.string().optional(),
   amount: z.number().optional(),
   trxType: z.enum(["income", "expense"]).optional()
