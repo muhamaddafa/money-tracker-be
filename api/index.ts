ed1d33d641c26a4d413bfa1f7177a2ecac690053
muhamaddafa <gaming102dafa@gmail.com>
2025-10-29 22:58:16 +0700

    s

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import { Elysia } from "elysia";
-import { api } from "../src/modules/api";
+import api from "../src/modules/api";
 
 export default new Elysia()
   .get("/", () => "Welcome to the Bun Elysia API!")
