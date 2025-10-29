651b532f4ec05ca6806180e263f4f9ef9e915e31
muhamaddafa <gaming102dafa@gmail.com>
2025-10-29 22:42:25 +0700

    b

--- src/modules/api.ts
+++ src/modules/api.ts
@@ -5,7 +5,7 @@ import { categoriesAPI } from "./categories";
 import { CustomHttpError } from "../libs/ResponseHandler";
 import { accountsAPI } from "./accounts";
 
-export const api = new Elysia({prefix: "/api"})
+export const api = new Elysia({prefix: "/track"})
   .use(transactionsAPI)
   .use(balanceAPI)
   .use(categoriesAPI)
